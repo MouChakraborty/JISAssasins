7e7b1cc782f8dad4ef04c19a2cbc0a624f6df20b
MouChakraborty <46235858+MouChakraborty@users.noreply.github.com>
2018-12-29 13:02:15 +0530

    Add files via upload

---
+++ Question15.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Program to find the value of x^(y+z) By Moumita Chakraborty
+import math
+x=int(input("Enter the value of x"))
+y=int(input("Enter the value of y"))
+z=int(input("Enter the value of z"))
+a=y+z
+b=pow(x,a)
+print("The value is",b)
+
+
+# In[ ]:
+
+
+
+
