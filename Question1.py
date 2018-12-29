20a8702559fe2d778d81d6647dd3b24050e615ec
MouChakraborty <46235858+MouChakraborty@users.noreply.github.com>
2018-12-29 11:59:56 +0530

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Program to accept four digit number from user and count zero,odd and even digits from the entered number By Moumita Chakraborty
+class Count:
+    def counting(self,num):
+        x=num
+        i=0
+        e=0
+        o=0
+        z=1
+        count=4
+        while(count>0):
+            z=x%10
+            if z==0:
+                i+=1
+            elif z%2==0:
+                e+=1
+            elif z%2 !=0:
+                o+=1
+            x=x//10
+            count-=1
+        print("Zeroes={},Evens={}, Odds={}".format(i,e,o))
+        
+c=Count()
+num=int(input("Enter a four digit number:"))
+c.counting(num)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
